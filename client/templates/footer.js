783c7a3dc2d7a6ab4888b15837be061fdee23855
yzhou <yzhou@elijah.com.cn>
2015-05-27 17:42:37 +0800

    增加footer功能
    上一章
    下一章
    播放、暂停

--- client/templates/footer.js
+++ client/templates/footer.js
@@ -1,5 +1,17 @@
-Template.footer.events({
-	'click button': function () {
+Session.setDefault('isPlaying', false);   //当前是否正在播放
 
+Template.footer.helpers({
+	isPlaying: function () {
+		return Session.get('isPlaying');
+	}
+});
+
+Template.footer.events({
+	'click button[data-skipbackward]': function () {
+		lastChapter();
+	},'click button[data-play]': function () {
+		Session.set('isPlaying', ! Session.get('isPlaying'));
+	},'click button[data-skipforward]': function () {
+		nextChapter();
 	}
 });

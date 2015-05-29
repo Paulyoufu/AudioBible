ecf8d7941fec07367213026c3298a07dc66459a1
yzhou <yzhou@elijah.com.cn>
2015-05-29 11:03:49 +0800

    修复经文跳动bug

--- client/lib/bibleScroll.js
+++ client/lib/bibleScroll.js
@@ -1,10 +1,18 @@
 /**
  * Created by paul on 15/5/27.
  */
-BibleScroll=function(SectionIndex){
+Session.setDefault('sectionIndex', 0);        //当前播放的section索引
+
+BibleScroll=function(sectionIndex){
+
+	if(sectionIndex == Session.get('sectionIndex')){
+		return;
+	}
+	Session.set('sectionIndex', sectionIndex);
+
     var contentHeight=window.screen.height/2;
-    var centreBible=$("#divBible span:eq("+SectionIndex+")").position().top-contentHeight;
+    var centreBible=$("#divBible span:eq("+sectionIndex+")").position().top-contentHeight;
     $("#divBible").scrollTop(centreBible+150);
     $("#divBible span").removeClass("green");
-    $("#divBible span:eq("+SectionIndex+")").addClass("green");
+    $("#divBible span:eq("+sectionIndex+")").addClass("green");
 }

c960f513ae66fd38387be43bcf5bdd5ecf45ca13
Paulyoufu <liangqiangcsharp@qq.com>
2015-06-01 09:27:24 +0800

    scroll Ok
    可以移动，但是遇到第二章第8节的时候就结束，没有收到sectionIndex还得需要再调度一下

--- client/lib/bibleScroll.js
+++ client/lib/bibleScroll.js
@@ -9,10 +9,22 @@ BibleScroll=function(sectionIndex){
 		return;
 	}
 	Session.set('sectionIndex', sectionIndex);
-
+    console.log(sectionIndex+".....................111111111111");
+   // var contentHeight=window.screen.height/2;
+   // var centreBible=$("#divBible p:eq("+sectionIndex+")").position().top-contentHeight;
+   // $("#divBible").scrollTop(centreBible+150);
+   // $("#divBible p").removeClass("green");
+   // $("#divBible p:eq("+sectionIndex+")").addClass("green");
     var contentHeight=window.screen.height/2;
-    var centreBible=$("#divBible span:eq("+sectionIndex+")").position().top-contentHeight;
-    $("#divBible").scrollTop(centreBible+150);
+    var centreBible=$("#divBible span:eq("+sectionIndex+")").offset().top-contentHeight;
+    console.log(centreBible+"........1..................");
+    $("#divBible").scrollTop(centreBible);
     $("#divBible span").removeClass("green");
     $("#divBible span:eq("+sectionIndex+")").addClass("green");
+    if(contentHeight>0)
+    {centreBible=$("#divBible span:eq("+sectionIndex+")").offset().top;
+        $("#divBible").scrollTop(centreBible+$("#divBible span:eq("+sectionIndex+")").height()-330);
+    }
+    console.log(centreBible+$("#divBible span:eq("+sectionIndex+")").height()-360+".....................position");
+
 }

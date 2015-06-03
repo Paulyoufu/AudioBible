a2c97ea780aaec745d1084f8c1b602481dd45411
Paulyoufu <liangqiangcsharp@qq.com>
2015-06-03 18:17:17 +0800

    removeCss Blue

--- client/lib/bibleScroll.js
+++ client/lib/bibleScroll.js
@@ -2,37 +2,34 @@
  * Created by paul on 15/5/27.
  */
 Session.setDefault('sectionIndex', null);        //当前播放的section索引
-Session.setDefault("sumSection",0);
+Session.setDefault("sumSectionHeight",0);
 Session.setDefault("scrollPosition",0);
 
 BibleScroll=function(sectionIndex){
+
 	if(sectionIndex == Session.get('sectionIndex')){//scrollPosition=0;
 		return;
 	}
+    console.log(sectionIndex+"____________ index_________")
 	Session.set('sectionIndex', sectionIndex);
-        if(Session.get("sumSection") < window.screen.height/2)
-        {  // console.log(window.screen.height/2+"===========888111333555555555555555555==========");
-            Session.set("sumSection",Session.get("sumSection") + $("#divBible span:eq(" + sectionIndex + ")").outerHeight(true));
-          //  console.log(Session.get("sumSection")+"++++sum++++");
+        if(Session.get("sumSectionHeight") < window.screen.height/2-50)
+        {   console.log(window.screen.height/2+"===========8881115667777777788888888888=========");
+            Session.set("sumSectionHeight",Session.get("sumSectionHeight") + $("#divBible span:eq(" + sectionIndex + ")").outerHeight(true));
+           console.log(Session.get("sumSectionHeight")+"++++sum++++");
             $("#divBible span").removeClass("blue");
             $("#divBible span:eq(" + sectionIndex + ")").addClass("blue");
         }
         else{
-           Session.set("scrollPosition", Session.get("scrollPosition")+$("#divBible span:eq(" + sectionIndex + ")").outerHeight());
-         //   console.log($("#divBible span:eq(" + sectionIndex + ")").outerHeight()+"    hhhhhhhhhhhhhhhhhhhh");
+           Session.set("scrollPosition", Session.get("scrollPosition")+$("#divBible span:eq(" + sectionIndex + ")").outerHeight());console.log($("#divBible span:eq(" + sectionIndex + ")").outerHeight()+"    hhhhhhhhhhhhhhhhhhhh");
             $("#divBible").scrollTop(Session.get("scrollPosition")) ;
             $("div span").removeClass("blue");
             $("#divBible span:eq(" + sectionIndex + ")").addClass("blue");
-           // console.log($("#divBible span:eq(" + sectionIndex + ")").position().top);
+            console.log($("#divBible span:eq(" + sectionIndex + ")").position().top);
 
         }
-
-
 }
-
-
 BibleScrollTop=function(){
     $("#divBible").scrollTop($("div span:eq(0)").position().top);
-    Session.set("sumSection",0);
+    Session.set("sumSectionHeight",0);
     Session.set("scrollPosition",0);
 }

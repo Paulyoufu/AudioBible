0ee02f21f1275c811fbff5195896f74f853514ee
yzhou <yzhou@elijah.com.cn>
2015-06-06 23:21:49 +0800

    add collection

--- both/collections.js
+++ both/collections.js
@@ -1,22 +1,31 @@
 Posts = new Mongo.Collection('posts');
+IP = new Mongo.Collection('ip');
 
-Posts.attachSchema(new SimpleSchema({
-  bookSN: {
-    type: Number
-  },
-  chapterSN: {
-    type: Number
-  },
-  createTime: {
-    type: String
-  }
-}));
+// Posts.attachSchema(new SimpleSchema({
+//   bookSN: {
+//     type: Number
+//   },
+//   chapterSN: {
+//     type: Number
+//   },
+//   createTime: {
+//     type: String
+//   }
+// }));
 
 SendMsg = function (bookSN, chapterSN) {
     var myDate = new Date();
     Posts.insert({
       bookSN: bookSN,
       chapterSN: chapterSN,
-      createTime: 'myDate.toLocaleTimeString()'
+      createTime: myDate.toLocaleString()
+    });
+}
+
+SendIP = function (ip) {
+    var myDate = new Date();
+    IP.insert({
+        ip: ip,
+        createTime: myDate.toLocaleString()
     });
 }
